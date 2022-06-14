f89e4d8c0cef4215d31babb13a1035a1c6ea4053
Alukartd77 <105003977+Alukartd77@users.noreply.github.com>
2022-06-14 11:24:59 -0400

    Update Evaluacion.py

--- Evaluacion.py
+++ Evaluacion.py
@@ -1,30 +1,31 @@
 while True:
-
+#aqui inicia el bucle y el codigo
     print("""
     dime, cuantos numeros deseas ingresar:
     1) quiero ingresar 2 numeros
     2) quiero ingresar 3 numeros
     3) terminar codigo
     """)
+    #Estas son las opciones 
     opcion= int(input("elige una opcion: "))
 
-    if opcion == 1:
-        n1 = float(input("primer numero"))
-        n2 = float(input("segundo numero"))
-        if n1 > n2:
-            print("el segundo numero NO es mayor que el primero")
+    if opcion == 1:    #esta es la primera opcion del codigo
+        n1 = float(input("primer numero"))  #se pide ingresar el primer numero
+        n2 = float(input("segundo numero")) #se pide ingresar el segundo numero
+        if n1 > n2:  #aqui se decide si el segundo numero NO es mayor que el primero 
+            print("el segundo numero NO es mayor que el primero")  #este print sale si la condicion se cumple 
             
 
-    elif opcion == 2:
-        n1 = float(input("primer numero"))
-        n2 = float(input("segundo numero"))
-        n3 = float(input("tercer numero"))
-        if n1>n2 or n1>n3:
+    elif opcion == 2:   #esta es la segunda opcion del codigo
+        n1 = float(input("primer numero")) #se pide ingresar el primer numero
+        n2 = float(input("segundo numero")) #se pide ingresar el segundo numero
+        n3 = float(input("tercer numero")) #se pide ingresar el tercer numero
+        if n1>n2 or n1>n3:  #aqui se decide si el segundo y tercer numero NO es mayor que el primero 
             print("el segundo numero NO es mayor que el primero")
             print("el tercer numero NO es mayor que el primero")
-
+            #Estos prints aparecen si las codiciones se cumplen
 
     elif opcion == 3:
-        break
+        break  #aqui se quiebra el codigo y se termina el bucle 
     else:
-        print("opcion incorrecta")
+        print("opcion incorrecta")  #esta es una opcion incorrecta
